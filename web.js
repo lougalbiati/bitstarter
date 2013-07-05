3d5e301f35caedcb6dee02e684df99fde4048f76
Lou Galbiati <lougalbiati@yahoo.com>
2013-07-04 21:56:00 -0700

    web.js dont throw err

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var nchar = fs.readFileSync("index.html", function (err, data) {
-    if (err) throw err;
+    // if (err) throw err;
     // console.log(data);
     response.send('Hello World 4!');
     //response.send(data);
