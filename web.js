844b43413dcfb7b73664df20db4f39ba1c573cdf
Lou Galbiati <lougalbiati@yahoo.com>
2013-07-04 22:13:20 -0700

    web.js remove non-working commented stuff

--- web.js
+++ web.js
@@ -3,18 +3,8 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-
 // app.get('/', function(request, response) {
 //   response.send('Hello World 4!');
-// });
-// app.get('/', function(request, response) {
-//   var nchar = fs.readFileSync("index.html", function (err, data) {
-//     // if (err) throw err;
-//     // console.log(data);
-//     response.send('Hello World 4!');
-//     //response.send(data);
-//     //response.send(data.toString('utf8'));
-//   });
 // });
 app.get('/', function(request, response) {
   var buf = fs.readFileSync("index.html");
